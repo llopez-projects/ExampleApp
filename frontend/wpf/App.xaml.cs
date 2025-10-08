d16e6671ce4d21d745eea858091cecdcc187a403
llopez-projects <lopez.leonardo@msn.com>
2025-10-08 09:12:56 -0300

    Frontend connect to api

--- frontend/wpf/App.xaml.cs
+++ frontend/wpf/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Configuration;
+﻿using core.Interfaces;
+using core.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,5 +12,21 @@ namespace wpf;
 /// </summary>
 public partial class App : Application
 {
+    public static IServiceProvider ServiceProvider { get; private set; }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var services = new ServiceCollection();
+
+        // Configurar HttpClient con base address
+        services.AddHttpClient<IEmployeeService, EmployeeService>(client =>
+        {
+            client.BaseAddress = new Uri("http://localhost:5026/");
+        });
+
+        ServiceProvider = services.BuildServiceProvider();
+
+        base.OnStartup(e);
+    }
 }
 
--- frontend/wpf/MainWindow.xaml.cs
+++ frontend/wpf/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using core.DTOs;
+using core.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace wpf;
@@ -8,19 +11,30 @@ namespace wpf;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private readonly IEmployeeService _employeeService;
+    private EmployeeDto _employee;
     public MainWindow()
     {
-        InitializeComponent();       
+        InitializeComponent();
+        _employeeService = App.ServiceProvider.GetRequiredService<IEmployeeService>();
     }
 
-    private void Guardar_Click(object sender, RoutedEventArgs e)
+    private async void Window_Loaded(object sender, RoutedEventArgs e)
     {
-        
-    }    
+        var employees = await _employeeService.GetAllAsync();
+        lstEmployees.ItemsSource = employees;
+    }
 
     private void lstEmployees_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-
+        _employee = lstEmployees.SelectedItem as EmployeeDto;
+        if (_employee != null)
+        {
+            txtFullName.Text = _employee.FullName;
+            txtEmail.Text = _employee.Email;
+            txtDateHired.Text = _employee.DateHired.ToString();
+            txtDepartment.Text = _employee.DepartmentName;
+        }
     }
 }
 
